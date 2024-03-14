a2e752cf4f4cef8c0c7fe842cc065b2cab52b666
Louis Chemineau <louis@chmn.me>
2024-03-14 10:16:43 +0100

    Rename MetadataFileEvents to VersionAuthorListener
    Signed-off-by: Louis Chemineau <louis@chmn.me>

--- apps/files_versions/lib/AppInfo/Application.php
+++ apps/files_versions/lib/AppInfo/Application.php
@@ -36,7 +36,7 @@ use OCA\Files_Versions\Capabilities;
 use OCA\Files_Versions\Listener\FileEventsListener;
 use OCA\Files_Versions\Listener\LoadAdditionalListener;
 use OCA\Files_Versions\Listener\LoadSidebarListener;
-use OCA\Files_Versions\Listener\MetadataFileEvents;
+use OCA\Files_Versions\Listener\VersionAuthorListener;
 use OCA\Files_Versions\Versions\IVersionManager;
 use OCA\Files_Versions\Versions\VersionManager;
 use OCP\Accounts\IAccountManager;
